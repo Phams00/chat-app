1e2574146c9c72b147a6a97c467204903ea74455
Phams00 <zeffanonevan@gmail.com>
2026-09-26 19:59:09 +0700

    feat: middleware sistem token untuk login fix: api tidak tersambung

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const authMiddleware = require('../middlewares/auth.middleware');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+router.get('/me', authMiddleware, (req, res) => {
+    res.json({ userId: req.user.id, message: 'user authenticated'})
+});
 
 module.exports = router;
